c16130f373930275e7932e159c780ec257810a66
Josh Pollock <jpollock412@gmail.com>
2016-06-18 16:17:11 -0400

    prepare for making a v2

--- core.php
+++ core.php
@@ -10,9 +10,9 @@
  * Plugin Name: CalderaWP License Manager
  * Plugin URI:  http://CalderaWP.com
  * Description: License manager for CalderaWP Plugins
- * Version:     1.2.3
- * Author:      David Cramer for CalderaWP LLC<David@CalderaWP.com>
- * Author URI:  http://digilab.co.za/
+ * Version:     2.0.0-a-1
+ * Author:      CalderaWP LLC
+ * Author URI:  https://CalderaWP.com/
  * Text Domain: calderawp-license-manager
  * License:     GPL-2.0+
  * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
@@ -30,23 +30,18 @@ if( defined( 'CALDERA_WP_LICENSE_MANAGER_VER' ) ){
 
 define( 'CALDERA_WP_LICENSE_MANAGER_PATH', plugin_dir_path( __FILE__ ) );
 define( 'CALDERA_WP_LICENSE_MANAGER_URL', plugin_dir_url( __FILE__ ) );
-define( 'CALDERA_WP_LICENSE_MANAGER_VER', '1.2.3' );
+define( 'CALDERA_WP_LICENSE_MANAGER_VER', '2.0.0-a-1' );
 define( 'CALDERA_WP_LICENSE_MANAGER_BASENAME', plugin_basename( __FILE__ ) );
 
 // load internals
-require_once( CALDERA_WP_LICENSE_MANAGER_PATH . 'classes/class-core.php' );
-require_once( CALDERA_WP_LICENSE_MANAGER_PATH . 'classes/class-options.php' );
-require_once( CALDERA_WP_LICENSE_MANAGER_PATH . 'classes/class-feed.php' );
 
-require_once( CALDERA_WP_LICENSE_MANAGER_PATH . 'classes/class-settings.php' );
 require_once( CALDERA_WP_LICENSE_MANAGER_PATH . 'includes/functions.php' );
 
 require_once( CALDERA_WP_LICENSE_MANAGER_PATH . 'classes/updater/class-updater.php' );
 require_once( CALDERA_WP_LICENSE_MANAGER_PATH . 'classes/updater/class-plugin-updates.php' );
 require_once( CALDERA_WP_LICENSE_MANAGER_PATH . 'classes/updater/class-updater-options.php' );
 
-// Load instance
-add_action( 'plugins_loaded', array( 'CalderaWP_License_Manager', 'get_instance' ) );
+
 
 //load dissmissable notices
 add_action( 'plugins_loaded', 'cwp_license_manager_load_dismissible_notices', 0 );
