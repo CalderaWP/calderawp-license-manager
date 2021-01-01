10a552cf85e061a30c3c3620c832fac7c0df2621
New0 <nico.figueira.mahe@gmail.com>
2021-01-01 11:30:37 +0100

    Change links to calderaforms.com account

--- includes/templates/main-ui.php
+++ includes/templates/main-ui.php
@@ -36,8 +36,8 @@
 		<!--
 		<li class="calderawp_license_manager-nav-tab" style="margin-left: 4px">
 			<?php printf( 'See your purchase history at %s', sprintf(
-					'<a href="https://calderawp.com/checkout/purchase-history/" title=%2s" target="_blank">CalderaWP.com/checkout/purchase-history</a>',
-						__( 'CalderaWP Purchase History', 'calderawp-license-manager' )
+					'<a href="https://calderaforms.com/account/" title=%2s" target="_blank">https://calderaforms.com/account/</a>',
+						__( 'CalderaForms Purchase History', 'calderawp-license-manager' )
 					)
 				);
 			?>
